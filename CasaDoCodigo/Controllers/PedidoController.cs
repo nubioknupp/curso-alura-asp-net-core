5d59be268bc44c6179704474e766958c0d8846e1
Núbio Knupp <nubioknupp@gmail.com>
2017-06-15 22:45:51 -0300

    Adicionei a página que lista os produtos

---
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        // GET
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+
+         }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+
+   
+      }
+}
